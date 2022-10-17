0bcbcf681e38789f1bf03c5fcc9d1716f9b65eed
Herve <herve@example.com>
2022-10-17 11:50:01 +0200

    init projet QueryPrepare

---
+++ getVillesV1.php
@@ -0,0 +1,23 @@
+<?php
+//V1 requete QUERY
+$dsn = 'mysql:dbname=voyage;host=localhost';
+try {
+    $db = new PDO($dsn, 'root', '');  //connexion MySQL Laragon
+    echo 'connexion dbvoyage OK';
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+echo '<h3>Voir une ville</h3>';
+//parametre idVille méthode GET (ou POST si formulaire)
+$idVille=$_GET['idVille'];
+$txtReq="select id,nom from ville where id=$idVille";
+$req = $db->query($txtReq);
+$uneVille = $req->fetch(PDO::FETCH_ASSOC);
+while ($uneVille != FALSE) {
+    foreach ($uneVille as $cle => $valeur) {
+        echo $cle, ' : ', $valeur, ' ';
+    }    
+    echo '<br>';
+    $uneVille = $req->fetch(PDO::FETCH_ASSOC);
+}
+?>
